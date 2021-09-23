2e86eee45eadc228ebeb5edc12fe7cb567515475
Jože Fortun <joze.fortun@gmail.com>
2021-09-23 15:12:57 +0200

    Update display element thumbnails

--- src/assets/DisplayElements/index.js
+++ src/assets/DisplayElements/index.js
@@ -1,4 +1,4 @@
-export const Text = require('./Placeholder.jpg')
-export const Table = require('./Placeholder.jpg')
-export const Chart = require('./Placeholder.jpg')
-export const Metric = require('./Placeholder.jpg')
+export const Text = require('./Text.png')
+export const Table = require('./Table.png')
+export const Chart = require('./Chart.png')
+export const Metric = require('./Metric.png')

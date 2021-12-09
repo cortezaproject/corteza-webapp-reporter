d250827bda9cf106b5a46c29a106421bcbd757e1
Tomaž Jerman <tomaz.jerman121@gmail.com>
2021-12-09 15:01:08 +0100

    Fix improper route registration

--- src/views/routes.js
+++ src/views/routes.js
@@ -6,8 +6,8 @@ export default [
     component: () => import('./Layout'),
     children: [
       { name: 'report.list', path: 'list', component: () => import('./Report/List') },
-      { name: 'report.view', path: '/:reportID', component: () => import('./Report/View') },
       { name: 'report.create', path: 'new', component: () => import('./Report/Edit') },
+      { name: 'report.view', path: '/:reportID', component: () => import('./Report/View') },
       { name: 'report.edit', path: '/:reportID/edit', component: () => import('./Report/Edit') },
       { name: 'report.builder', path: '/:reportID/builder', component: () => import('./Report/Builder') },
     ],

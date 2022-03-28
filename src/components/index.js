6dad760fafb2daad48c797d61377c4c21c75464b
Jože Fortun <joze.fortun@gmail.com>
2022-03-28 11:40:11 +0200

    Remove redundant vue tippy

--- src/components/index.js
+++ src/components/index.js
@@ -1,14 +1,11 @@
 import Vue from 'vue'
 import { FontAwesomeIcon, FontAwesomeLayers } from '@fortawesome/vue-fontawesome'
 import PortalVue from 'portal-vue'
-import VueTippy, { TippyComponent } from 'vue-tippy'
 import './faIcons'
 import { components } from '@cortezaproject/corteza-vue'
 
-Vue.use(VueTippy)
 Vue.use(PortalVue)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
 Vue.component('c-permissions-button', components.CPermissionsButton)
 Vue.component('c-input-confirm', components.CInputConfirm)
-Vue.component('tippy', TippyComponent)

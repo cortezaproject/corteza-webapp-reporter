89f1abec9508378982bb727b9c63eb012e65ce17
Jože Fortun <joze.fortun@gmail.com>
2022-07-27 15:10:25 +0200

    Switch from chartjs to echarts

--- src/components/index.js
+++ src/components/index.js
@@ -4,6 +4,47 @@ import PortalVue from 'portal-vue'
 import './faIcons'
 import { components } from '@cortezaproject/corteza-vue'
 
+// import ECharts modules manually to reduce bundle size
+import ECharts from 'vue-echarts'
+import { use } from 'echarts/core'
+import {
+  SVGRenderer,
+} from 'echarts/renderers'
+import {
+  LineChart,
+  BarChart,
+  PieChart,
+  GaugeChart,
+  HeatmapChart,
+  FunnelChart,
+} from 'echarts/charts'
+import {
+  TitleComponent,
+  GridComponent,
+  LegendComponent,
+  TooltipComponent,
+  VisualMapComponent,
+  ToolboxComponent,
+} from 'echarts/components'
+
+use([
+  BarChart,
+  LineChart,
+  PieChart,
+  GaugeChart,
+  HeatmapChart,
+  FunnelChart,
+  SVGRenderer,
+  TitleComponent,
+  GridComponent,
+  TooltipComponent,
+  LegendComponent,
+  VisualMapComponent,
+  ToolboxComponent,
+])
+
+Vue.component('e-charts', ECharts)
+
 Vue.use(PortalVue)
 Vue.component('font-awesome-icon', FontAwesomeIcon)
 Vue.component('font-awesome-layers', FontAwesomeLayers)
